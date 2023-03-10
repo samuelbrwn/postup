7ea2e609c5cef120914d2ee29c1143e1fe4bd5e8
Samuel Brown <samuelbrwn97@gmail.com>
2023-03-10 14:43:13 -0700

    handling cmd line args and printing to a file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let filename = &args[1];
+    let txt = &args[2];
+    fs::write(filename, txt)
+        .expect("Expected to open file.");
 }
