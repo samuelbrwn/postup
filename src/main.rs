f997ca4781f7cb4ac4b672a69cc8431cd8357f06
Samuel Brown <samuelbrwn97@gmail.com>
2023-03-10 21:25:00 -0700

    added functionality. prints messages to a file

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
 use std::env;
+use std::io::Write;
 use std::fs;
+use chrono::prelude::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let utc: DateTime<Local> = Local::now();
 
     let filename = &args[1];
     let txt = &args[2];
-    fs::write(filename, txt)
-        .expect("Expected to open file.");
+
+    let msgcontent = format!("POSTD@{}:\n\t-{}\n\n", utc, txt);
+
+    let mut file = fs::OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(&filename)
+        .expect("Unable to open.");
+
+    file.write_all(msgcontent.as_bytes()).expect("write failed."); 
+    
+
 }
